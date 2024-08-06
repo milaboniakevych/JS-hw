643d5f29c2e237c7aa92ba9099fafe1c9dbdf149
lboniakevych.knu18@gmail.com <lboniakevych.knu18@gmail.com>
2024-08-06 12:43:02 +0300

    06.08

---
+++ JS-1/main.js
@@ -0,0 +1,64 @@
+let word1 = 'hello';
+console.log(word1);
+
+let word2 = 'owu';
+console.log(word2);
+
+let word3 = 'com';
+console.log(word3);
+
+let word4 = 'ua';
+console.log(word4);
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+let num5 = 3.14;
+console.log(num5);
+
+let num6 = 2.7;
+console.log(num6);
+
+let num7 = 16;
+console.log(num7);
+
+let bool1 = true;
+console.log(bool1);
+
+let bool2 = false;
+console.log(bool2);
+
+let firstName = 'Liudmyla';
+let middleName = 'Oleksandrivna';
+let lastName = 'Boniakevych';
+let space = ' '
+
+let person = firstName + space + middleName + space + lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let firstname = prompt('Your name');
+console.log(firstname);
+
+let middlename = prompt('Your middle name');
+console.log(middlename);
+
+let age = prompt('Your age');
+console.log(age);
